d3417c44b2ad9c12276da6756e1e38383d408ff3
Кружков Владимир <morglod@gmail.com>
2019-09-20 03:47:42 +0300

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: null,
@@ -27,17 +27,17 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    // "json",
+    "text",
+    "lcov",
+    // "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: null,
@@ -64,9 +64,9 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: [
+    "node_modules"
+  ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -115,9 +115,9 @@ module.exports = {
   // rootDir: null,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "<rootDir>"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -141,10 +141,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.js",
+    "**/__tests__/*.js",
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
---
+++ src/__tests__/test1.ts
@@ -0,0 +1,34 @@
+import { Api } from "../api";
+import { simpleCrossStream } from "../streams/utils";
+import { ITransportProtocol } from "../transports/itransport";
+import { DuplexStreamTransport } from "../transports/duplex-stream";
+
+function remoteSum(a: number, b: number, sumCallback: (result: number) => void, mulCallback: (result: number) => void) {
+    console.log(`remoteSum(${a}, ${b})`);
+    sumCallback(a + b);
+    mulCallback(a * b);
+    mulCallback(a * b + 5);
+    return 'hello world!';
+}
+
+it('main', async () => {
+    const session = simpleCrossStream<ITransportProtocol>();
+
+    const localStreamTransport = new DuplexStreamTransport(session.a, undefined, 'local');
+    const localApi = new Api<{ remoteSum: typeof remoteSum }, {}>({}, localStreamTransport);
+
+    const remoteStreamTransport = new DuplexStreamTransport(session.b, undefined, 'remote');
+    const remoteApi = new Api<{}, { remoteSum: typeof remoteSum }>({
+        remoteSum,
+    }, remoteStreamTransport);
+
+    const result = await localApi.callMethod(
+        'remoteSum',    // Remote api method
+        10,             // argument `a`
+        20,             // argument `b`
+        sumResult => console.log('sum:', sumResult),    // argument `sumCallback`
+        mulResult => console.log('mul:', mulResult)     // argument `mulCallback`
+    );
+
+    console.log('result', result);
+});
